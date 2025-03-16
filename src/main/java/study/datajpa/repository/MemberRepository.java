fee7345c032de2c4662b550ef9cf13d8fd06c578
omea92 <thdjun92@naver.com>
2025-03-16 21:18:58 +0900

    JpaRepository 기능 주석 정리

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,5 +1,6 @@
 package study.datajpa.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
@@ -7,5 +8,6 @@ import study.datajpa.entity.Member;
 // JPA CRUD 기본 제공
 // CrudRepository ( CRUD ), JpaRepository ( CRUD + PAGING )
 public interface MemberRepository extends JpaRepository<Member, Long> {
-
+    //쿼리 메소드 기능 제공으로 자동 쿼리 지원하는데 문법이 맞아야 한다(자세한건 Spring Data Jpa 메뉴얼 사이트 참고) : findBy필드명And필드명GreaterThan
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 }
